5e338f3d3a3ae4e9ec22ba7d5028ba0113e4db3a
Sebastian <sebasromero2803@gmail.com>
2022-03-09 07:34:44 -0500

    dbz avanzando

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  personajes: Personaje[] = [
+    {
+      nombre: 'Krillin',
+      poder: 700
+    },
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ];
+
+}
